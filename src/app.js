0d3dbedda82fc02e3ff5ba4fbfc118495d4f6678
Pineapple-Geek <naim.jebbour@outlook.fr>
2026-08-29 15:10:23 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -86,17 +86,21 @@ app.listen(port, () => {
 // LISTE DES PKG
 // ------------------------------------------------------------
 function get_pkgs() {
-  const walkSync = function (dir, filelist) {
+  const walkSync = function(dir, filelist) {
     const files = fs.readdirSync(dir);
-    files.forEach(function (file) {
-      const filepath = dir + '/' + file;
+    files.forEach(function(file) {
+      filepath = dir + '/' + file;
       const stat = fs.statSync(filepath);
       if (stat.isDirectory()) {
         filelist = walkSync(filepath, filelist);
       } else if (path.extname(file).toLowerCase() === '.pkg') {
-        filelist.push({
+        let dirname = path.dirname(filepath).replace(static_files_path + '/', '')
+        let root = dirname.split("/", 1)[0];
+        if (!filelist[root])
+          filelist[root] = [];
+        filelist[root].push({
           filepath: filepath,
-          dir: path.dirname(filepath),
+          dir: dirname.replace(root + '/', ''),
           name: path.basename(filepath),
           size: filesize(stat.size)
         });
@@ -104,7 +108,7 @@ function get_pkgs() {
     });
     return filelist;
   };
-  return walkSync(static_files_path, []);
+  return walkSync(static_files_path, {});
 }
 
 // ------------------------------------------------------------
